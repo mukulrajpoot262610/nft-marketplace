a57dbbec78beb4880e872e4950b35d8dbd83f625
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-05-28 18:15:18 +0530

    ADD: launch page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="bg-black flex justify-center items-start lg:items-center min-h-screen  text-white flex-col p-10 overflow-hidden">
+      <h1 className="text-3xl md:text-4xl font-bold lg:text-center">
+        Discover and Sell Rare NFTs
+      </h1>
+      <p className="text-5xl md:text-9xl text-left lg:text-center my-10 font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-blue-600 to-green-600">Coming Soon!</p>
+      <p className="fixed bottom-10 left-10 lg:left-1/2 lg:-translate-x-1/2 text-gray-400 pr-10">Made by Mukul Rajpoot</p>
+    </main>
   )
 }
